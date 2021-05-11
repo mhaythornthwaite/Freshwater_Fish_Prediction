6673feb11a7b1812ad533f2e906ba67f03539f0b
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2021-05-11 22:43:12 +0100

    Model Fix - Learning Rate & Loss Plot

--- 03_basic_nn_model_build.py
+++ 03_basic_nn_model_build.py
@@ -67,23 +67,67 @@ plt.imshow(im, cmap='gray', vmin=0, vmax=255)
 data_array = gen_data_array(label_paths, image_size)
 train_images, test_images, train_labels, test_labels = train_test_split(data_array, one_hot_labels, test_size=0.2)
 
+#train_images = train_images[:100]
+#train_labels = train_labels[:100]
 
 #---------------------------------- MODEL BUILD -------------------------------
 
 simple_model = keras.Sequential([
-    layers.Dense(256, activation='relu', name='layer1', input_shape=(image_vector_len,)),
-    layers.Dense(64, activation='relu', name='layer2'),
+    layers.Dense(192, activation='relu', name='layer1', input_shape=(image_vector_len,)),
+    layers.Dense(48, activation='relu', name='layer2'),
     layers.Dense(14, activation='softmax', name='layer3'),
     ])
- 
+
+#full list of keras optimisers: https://keras.io/api/optimizers/
+#low learning rate: https://stackoverflow.com/questions/41488279/neural-network-always-predicts-the-same-class
+optimiser = keras.optimizers.Adam(learning_rate=0.00001)
+optimiser = keras.optimizers.RMSprop(learning_rate=0.00001)
+
 simple_model.compile(loss='categorical_crossentropy',
-                     optimizer=keras.optimizers.Adam(),
+                     optimizer=optimiser,
                      metrics=["accuracy"]
                      )
 
 simple_model.summary()
 
-simple_model.fit(train_images, train_labels, epochs=5, batch_size=batch_size)
+history = simple_model.fit(train_images, 
+                           train_labels, 
+                           epochs=1000, 
+                           batch_size=batch_size,
+                           validation_data=(test_images, test_labels))
+
+
+#------------------------------- MODEL PERFORMANCE ----------------------------
+
+#--------- TRAINING & VALIDATION LOSS ---------
+
+#setting up plottable variables
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss = history_dict['val_loss']
+epochs = list(range(1, len(loss_values)+1))
+
+#fig setup including twin axis
+fig, ax = plt.subplots()
+fig.suptitle('Training & Validation Loss', y=0.95, fontsize=16, fontweight='bold')
+ax2 = ax.twinx()
+
+#plotting training and validation loss
+train_loss = ax.plot(epochs, loss_values, 'r', label='Training Loss')
+val_loss = ax2.plot(epochs, val_loss, 'b', label='Validation Loss')
+
+#setting axis limits
+ax.set_ylim([min(loss_values)-0.2, 2.8])
+ax2.set_ylim([min(val_loss)-1, min(val_loss)+10])
+
+#plotting legend
+lns = train_loss + val_loss
+labs = [l.get_label() for l in lns]
+ax.legend(lns, labs, loc=0)
+
+train_predictions = simple_model.predict(train_images[:100])
+
+
 
 #https://www.youtube.com/watch?v=J6Ok8p463C4
 

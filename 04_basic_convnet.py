bef4c26c6424b1ce548df5864e8e1fb3a4d564c5
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2021-11-08 19:38:22 +0000

    basic cnn improved figures and metrics

--- 04_basic_convnet.py
+++ 04_basic_convnet.py
@@ -24,7 +24,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
-from fish_functions import print_tf_setup, open_jpeg_as_np, gen_data_array_image
+from fish_functions import print_tf_setup, open_jpeg_as_np, gen_data_array_image, n_retraining
 
 plt.close('all')
 
@@ -50,6 +50,7 @@ image_size = (32, 32)
 input_shape = image_size + (3,)
 batch_size = 32
 num_classes = 14
+num_epochs = 100
 
 image_vector_len = image_size[0] * image_size[1]
 num_images = len(labels)
@@ -94,6 +95,21 @@ clf = model.fit(train_images,
                 batch_size=batch_size,
                 validation_data=(test_images, test_labels))
 
+metrics_dict = n_retraining(model=model, 
+                            n=10, 
+                            train_data=train_images, 
+                            train_labels=train_labels, 
+                            val_data=test_images,
+                            val_labels=test_labels,
+                            epochs=num_epochs,
+                            batch_size=batch_size,
+                            s=3,
+                            smooth=False)
+
+#looking at predictions on a collection of test and train images for basic inspection and QC.
+train_predictions = model.predict(train_images[:100])
+val_predictions = model.predict(test_images[:100])
+
 
 #------------------------------- MODEL PERFORMANCE ----------------------------
 
@@ -107,25 +123,21 @@ epochs = list(range(1, len(loss_values)+1))
 
 #fig setup including twin axis
 fig, ax = plt.subplots()
-fig.suptitle('Training & Validation Loss Basic Convnet', y=0.95, fontsize=14, fontweight='bold')
+fig.suptitle('Training & Validation Loss Basic Dense Network', y=0.95, fontsize=14, fontweight='bold')
 
 #plotting training and validation loss
-ax.plot(epochs, loss_values, 'b', label='Training Loss')
-ax.plot(epochs, val_loss, 'r', label='Validation Loss')
-ax.axhline(min(val_loss), c='r', alpha=0.3, ls='dashed', label='Min Validation Loss')
+ax.plot(epochs, metrics_dict['train_loss_mean'], 'b', label='Training Loss')
+ax.plot(epochs, metrics_dict['val_loss_mean'], 'r', label='Validation Loss')
+ax.plot(epochs, metrics_dict['val_loss_std_p'], label='_nolegend_', alpha=0)
+ax.plot(epochs, metrics_dict['val_loss_std_n'], label='_nolegend_', alpha=0)
+ax.fill_between(epochs, metrics_dict['val_loss_std_p'], metrics_dict['val_loss_std_n'], color='grey', alpha=0.15)
+ax.axhline(np.nanmin(metrics_dict['val_loss_mean']), c='r', alpha=0.3, ls='dashed', label='Min Validation Loss')
 
-#setting axis limits
-ax.set_ylim([min(loss_values)-0.5, min(loss_values)+8])
-
-#plotting legend
-ax.legend()
-
-#plotting axis labels
+#setting axis limits, labels and legend
+ax.set_ylim([np.nanmin(metrics_dict['train_loss_mean'])-0.5, np.nanmin(metrics_dict['train_loss_mean'])+8])
 ax.set_xlabel('Epochs')
 ax.set_ylabel('Loss')
-
-train_predictions = model.predict(train_images[:100])
-val_predictions = model.predict(test_images[:100])
+ax.legend()
 
 #--------- TRAINING & VALIDATION ACCURACY ---------
 
@@ -133,23 +145,26 @@ val_predictions = model.predict(test_images[:100])
 accuracy_values = history_dict['accuracy']
 val_accuracy = history_dict['val_accuracy']
 
-#fig setup including twin axis
+#fig setup
 fig2, ax = plt.subplots()
-fig2.suptitle('Training & Validation Accuracy Basic Convnet', y=0.95, fontsize=14, fontweight='bold')
+fig2.suptitle('Training & Validation Accuracy Basic Dense Network', y=0.95, fontsize=14, fontweight='bold')
 
-#plotting training and validation loss
-ax.plot(epochs, accuracy_values, 'b', label='Training Accuracy')
-ax.plot(epochs, val_accuracy, 'r', label='Validation Accuracy')
-ax.axhline(max(val_accuracy), c='r', alpha=0.3, ls='dashed', label='Max Validation Accuracy')
+#plotting training and validation accuracy
+ax.plot(epochs, metrics_dict['train_acc_mean'], 'b', label='Training Accuracy')
+ax.plot(epochs, metrics_dict['val_acc_mean'], 'r', label='Validation Accuracy')
+ax.plot(epochs, metrics_dict['val_acc_std_p'], label='_nolegend_', alpha=0)
+ax.plot(epochs, metrics_dict['val_acc_std_n'], label='_nolegend_', alpha=0)
+ax.fill_between(epochs, metrics_dict['val_acc_std_p'], metrics_dict['val_acc_std_n'], color='grey', alpha=0.15)
+
+#plotting accuracy lines
+ax.axhline(np.nanmax(metrics_dict['val_acc_mean']), c='r', alpha=0.3, ls='dashed', label='Max Validation Accuracy')
 ax.axhline(1/14, c='k', alpha=0.3, ls='dashed', label='Random Guess Accuracy')
 
-#setting axis limits
-ax.set_ylim([0,max(accuracy_values)+0.1])
-
-#plotting legend
+#plotting legend and setting limits
 ax.legend()
 ax.set(xlabel='Epochs',
        ylabel='Accuracy');
+ax.set_ylim([0,np.nanmax(metrics_dict['train_acc_mean'])+0.1])
 
 #https://stats.stackexchange.com/questions/282160/how-is-it-possible-that-validation-loss-is-increasing-while-validation-accuracy
 
